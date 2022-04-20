c0d6885f0248123a985de98f5947bcff596857c6
IqbalPuteh <Iqbalrp75@gmail.com>
2022-04-20 15:19:26 +0800

    1. Deleting unused classes and code 2. Add Project Solution files for VS 3. Add API Controller name TransactionControler

---
+++ WanluShopWebAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WanluShopWebAPI.Models;
+//using System;
+//using System.Collections.Generic;
+//using Microsoft.EntityFrameworkCore;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+
+
+namespace WanluShopWebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly WANLUSHOPWEBAPIDBWANLUCLOTHDBMDFContext _context;
+
+        public TransactionsController(WANLUSHOPWEBAPIDBWANLUCLOTHDBMDFContext context)
+        {
+            _context = context;
+        }
+
+        private bool TransactionExists(Guid id)
+        {
+            return _context.Transactions.Any(e => e.TransId == id);
+        }
+
+        // GET: api/Transactions
+
+        [HttpGet("{test}")]
+        public IEnumerable<Product> Get()
+        {
+
+            using (var Context = new WANLUSHOPWEBAPIDBWANLUCLOTHDBMDFContext())
+            {
+                return Context.Products.ToList();
+            }
+        }
+
+        [HttpGet("{1}CustomerFirstLast")]
+        public IEnumerable<CustomerFirstLast> GetCustomerFirstLast()
+        {
+            using (var Context = new WANLUSHOPWEBAPIDBWANLUCLOTHDBMDFContext())
+            {
+                return Context.CustomerFirstLasts.ToList();
+            }
+        }
+    }
+}
